a30e4512ec7c4a59e7e9ef917b4be40129732c97
muhammed rifan <rifanmuhammed173@gmail.com>
2023-12-12 00:00:46 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SMA';
+
+constructor(public translate:TranslateService){
+  translate.addLangs(["en","nl","es","de"])
+  translate.setDefaultLang("en")
+}
+
+switchLanguage(lang:string){
+this.translate.use(lang)
+
+}
+
 }
